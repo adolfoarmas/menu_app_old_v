3547d4b8949f22526d99040b50f2d7cbe2710df3
adolfoarmas <adolfoarmas.90@gmail.com>
2023-03-02 11:41:15 -0800

    feature: post, put dishes from client

--- p_menu/p_menu/settings.py
+++ p_menu/p_menu/settings.py
@@ -166,6 +166,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
